bb0eedab1cf2ae24d0fc4bdc5eb47536b02a6c03
rmelendez30 <rmelendez30@gmail.com>
2021-08-02 20:28:18 +0200

    creating initial pages and redux store

---
+++ src/actions/categoriesActions.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import {GET_CATEGORIES } from '../utils/constants';
+
+
+export const getAllCategories = () => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.get('/categores.json');
+            dispatch(loadCategories(data));
+        } catch (error) {
+            console.log('Error loading categories data. Try again later.');
+        }
+    };
+};
+
+
+export const loadCategories = (categories) => ({
+    type: GET_CATEGORIES,
+    categories
+  });
